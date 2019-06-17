cc2c5dd46cedc646d6232bc7275539b086265b49
Lorenzo Busin <lorenzo.busin@gmail.com>
2019-06-17 15:59:48 +0200

    CRUD op work on dynamodb

--- my-app/index.js
+++ my-app/index.js
@@ -1,11 +1,47 @@
-// index.js
+console.log('Loading function');
 
-const serverless = require('serverless-http');
-const express = require('express')
-const app = express()
+var AWS = require('aws-sdk');
+var dynamo = new AWS.DynamoDB.DocumentClient();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+/**
+ * Provide an event that contains the following keys:
+ *
+ *   - operation: one of the operations in the switch statement below
+ *   - tableName: required for operations that interact with DynamoDB
+ *   - payload: a parameter to pass to the operation being performed
+ */
+exports.handler = function(event, context, callback) {
+    //console.log('Received event:', JSON.stringify(event, null, 2));
 
-module.exports.handler = serverless(app);
+    var operation = event.operation;
+
+    if (event.tableName) {
+        event.payload.TableName = event.tableName;
+    }
+
+    switch (operation) {
+        case 'create':
+            dynamo.put(event.payload, callback);
+            break;
+        case 'read':
+            dynamo.get(event.payload, callback);
+            break;
+        case 'update':
+            dynamo.update(event.payload, callback);
+            break;
+        case 'delete':
+            dynamo.delete(event.payload, callback);
+            break;
+        case 'list':
+            dynamo.scan(event.payload, callback);
+            break;
+        case 'echo':
+            callback(null, "Success");
+            break;
+        case 'ping':
+            callback(null, "pong");
+            break;
+        default:
+            callback('Unknown operation: ${operation}');
+    }
+};
---
+++ serverless-dynamodb-streams/handler.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+
+module.exports.updateUser = (event, context, callback) => {
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+  const data = event;
+  data.updatedAt = new Date().getTime();
+
+  const params = {
+    TableName: 'users',
+    Item: data
+  };
+
+  return dynamoDb.put(params, (error, data) => {
+    if (error) {
+      callback(error);
+    }
+    callback(null, { message: 'User successfully updated', params });
+  });
+};
+
+module.exports.createUser = (event, context, callback) => {
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+  const data = event;
+  data.updatedAt = new Date().getTime();
+
+  const params = {
+    TableName: 'users',
+    Item: data
+  };
+
+  return dynamoDb.put(params, (error, data) => {
+    if (error) {
+      callback(error);
+    }
+    callback(null, { message: 'User successfully created', params });
+  });
+};
+
+module.exports.deleteUser = (event, context, callback) => {
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+  const data = event;
+  data.updatedAt = new Date().getTime();
+
+  const params = {
+    TableName: 'users',
+    Key:{
+      "id": event.id
+    },
+  };
+
+  return dynamoDb.delete(params, (error, data) => {
+    if (error) {
+      callback(error);
+    }
+    callback(null, { message: 'User successfully deleted', params });
+    });
+  };
+
+module.exports.logger = (event, context, callback) => {
+  // print out the event information on the console (so that we can see it in the CloudWatch logs)
+  console.log(`The following happend in the DynamoDB database table "users":\n${JSON.stringify(event.Records[0].dynamodb, null, 2)}`);
+
+  callback(null, { event });
+};

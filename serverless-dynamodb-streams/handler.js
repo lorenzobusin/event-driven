809308b55ca53f4e3e4fed74aebf88129ac0daa7
Lorenzo Busin <lorenzo.busin@gmail.com>
2019-06-18 15:53:37 +0200

    getUsere API + easyGUI

--- serverless-dynamodb-streams/handler.js
+++ serverless-dynamodb-streams/handler.js
@@ -2,24 +2,6 @@
 
 const AWS = require('aws-sdk');
 
-module.exports.updateUser = (event, context, callback) => {
-  const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-  const data = event;
-  data.updatedAt = new Date().getTime();
-
-  const params = {
-    TableName: 'users',
-    Item: data
-  };
-
-  return dynamoDb.put(params, (error, data) => {
-    if (error) {
-      callback(error);
-    }
-    callback(null, { message: 'User successfully updated', params });
-  });
-};
 
 module.exports.createUser = (event, context, callback) => {
   const dynamoDb = new AWS.DynamoDB.DocumentClient();
@@ -37,6 +19,57 @@ module.exports.createUser = (event, context, callback) => {
       callback(error);
     }
     callback(null, { message: 'User successfully created', params });
+  });
+};
+
+module.exports.getUser = (event, context, callback) => {
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+ // const data = event;
+ // data.updatedAt = new Date().getTime();
+
+  const params = {
+    TableName: 'users',
+    KeyConditionExpression: "#id = :userId",
+    ExpressionAttributeNames:{
+        "#id": "userId"
+    },
+    ExpressionAttributeValues: {
+        ":userId": event.userId
+    }
+  };
+
+  return dynamoDb.query(params, (error, data) => {
+    if (error) {
+      callback(error);
+    }
+      callback(null, { message: 'User: ', data });
+    });
+
+  /*return dynamoDb.get(params, (error, data) => {
+    if (error) {
+      callback(error);
+    }
+      callback(null, { message: 'User: ', params });
+    });*/
+};
+
+module.exports.updateUser = (event, context, callback) => {
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+  const data = event;
+  data.updatedAt = new Date().getTime();
+
+  const params = {
+    TableName: 'users',
+    Item: data
+  };
+
+  return dynamoDb.put(params, (error, data) => {
+    if (error) {
+      callback(error);
+    }
+    callback(null, { message: 'User successfully updated', params });
   });
 };
 
@@ -61,30 +94,11 @@ module.exports.deleteUser = (event, context, callback) => {
     });
 };
 
-module.exports.getUser = (event, context, callback) => {
-  const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-  const data = event;
-  data.updatedAt = new Date().getTime();
-
-  const params = {
-    TableName: 'users',
-    Key:{
-      "id": event.id
-    },
-  };
-
-  return dynamoDb.get(params, (error, data) => {
-    if (error) {
-      callback(error);
-    }
-      callback(null, { message: 'User: ', params });
-    });
-};
-
 module.exports.logger = (event, context, callback) => {
   // print out the event information on the console (so that we can see it in the CloudWatch logs)
   console.log(`The following happend in the DynamoDB database table "users":\n${JSON.stringify(event.Records[0].dynamodb, null, 2)}`);
 
   callback(null, { event });
 };
+
+

6041675dcbb48d8afe4de742b29412606db702ad
Lorenzo Busin <lorenzo.busin@gmail.com>
2019-07-09 20:32:52 +0200

    Fix await lmbdas

--- serverless-user-management/recovery_handler.js
+++ serverless-user-management/recovery_handler.js
@@ -1,22 +1,8 @@
-module.exports.recovery = (event, context, callback) => {
+module.exports.pushEventStoreToSQS = (event, context, callback) => {
   const AWS = require('aws-sdk');
-  const LAMBDA = new AWS.Lambda();
+  const SQS = new AWS.SQS();
   const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-  //CLEAR DB TABLES
-  var params = {
-    FunctionName: "serverless-user-management-dev-clearDB", 
-    InvocationType: "Event", 
-    LogType: "Tail", 
-    Payload: ""
-  };
-
-  LAMBDA.invoke(params, function(err, data) {
-    if (err) 
-      console.log(err);
-    else 
-      console.log('Clearing the DB...');
-  });
+  const LAMBDA = new AWS.Lambda();
 
   const queryParams = {
     TableName: 'eventStore',
@@ -37,9 +23,7 @@ module.exports.recovery = (event, context, callback) => {
       else{
         const stringedData = JSON.stringify(data);
         const parsedData = JSON.parse(stringedData);
-        console.log("data: " + stringedData);
-
-        var events = parsedData.Items; 
+        var events  = parsedData.Items;
         events.sort((a, b) => { //order events by timestamp from the oldest
           var a1 = a.timestamp, b1 = b.timestamp;
           if (a1 < b1) return -1;
@@ -47,23 +31,57 @@ module.exports.recovery = (event, context, callback) => {
           return 0;
         });
 
-        console.log(JSON.stringify(events));
-
-        events.forEach(item =>{
-          const params = {
-            FunctionName: item.lambda, 
-            InvocationType: "Event", 
-            LogType: "Tail", 
-            Payload: JSON.stringify(item.payload)
+        async function invokeLambdas() {
+          var i = 0;
+          while(i < events.length){
+            var params = {
+              FunctionName: events[i].lambda, 
+              InvocationType: "Event", 
+              LogType: "Tail", 
+              Payload: JSON.stringify(events[i].payload)
+            };
+            
+            await LAMBDA.invoke(params).promise();
+            i++;
           };
-          LAMBDA.invoke(params, function(err, data) {
-            if (err) 
-              console.log(err);
-          });
-        });
-      };
-    }
-  });
+        };
+      
+        invokeLambdas();
+      /*loop = async () => {
+      await new Promise(resolve => setTimeout(resolve, 500));
+      console.log(i);
+      LAMBDA.invoke(params);
+      loop();*/
+        
+        }
+      }
+
+     
+});
+};
+
+
+
+
+module.exports.mediatorRecovery = (event, context, callback) => {
+  const AWS = require('aws-sdk');
+  const LAMBDA = new AWS.Lambda();
+
+  const stringedBody = event.Records[0].body.toString('utf-8'); //read new event from SQS
+  const parsedBody = JSON.parse(stringedBody);
+  console.log("body: " + stringedBody);
+
+  var params = {
+    FunctionName: parsedBody.FunctionName, 
+    InvocationType: "Event", 
+    LogType: "Tail", 
+    Payload: JSON.stringify(parsedBody.Payload) //only string type
+   };
+
+   LAMBDA.invoke(params, function(err, data) {
+    if (err) 
+      console.log(err);
+    }); 
 };
 
 
